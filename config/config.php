2dc29fe4bf1128585af26b18ec0148ccb61bf991
Geraline Masalta <201980176@psu.palawan.edu.ph>
2021-11-01 12:31:31 +0800

    changes made

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/phpsandbox/logApp-Masalta/');
+	define('ROOT_URL', 'http://localhost/logApp-Masalta/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447240');
 	define('DB_PASS', 'EaHclNXz2B');
