5d1c96ffabef450a1bf0ff9db596984d14f6180b
Geraline Masalta <91036446+GMasalta@users.noreply.github.com>
2021-11-02 08:59:08 +0800

    changes made
    changes made

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-	define('ROOT_URL', 'http://localhost/logApp-Masalta/');
+	define('ROOT_URL', 'https://sheltered-river-65868.herokuapp.com/');
 	define('DB_HOST', 'sql6.freesqldatabase.com');
 	define('DB_USER', 'sql6447240');
 	define('DB_PASS', 'EaHclNXz2B');
